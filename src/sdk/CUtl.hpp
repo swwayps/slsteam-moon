e134ff3725e60d30484c2c0eccd550d2d7782c6b
Ace SLS <acesls@protonmail.com>
2026-07-22 11:37:09 +0000

    refactor(hooks): Replace IClientUGC- & IClientUserStats::RunIPCFrame with CSteamEngine::RunInterface
    (cherry picked from commit d0888d0efbd582b73eda4e944bd81b55260e34a9)

--- src/sdk/CUtl.hpp
+++ src/sdk/CUtl.hpp
@@ -14,3 +14,16 @@ struct CUtlVector
 	CUtlMemory<T> memory;
 	int size;
 };
+
+class CUtlBuffer
+{
+public:
+
+	CUtlMemory<uint8_t> mem;	//0x0
+	int32_t get;				//0xC
+	int32_t put;				//0x10
+	int32_t offset;				//0x14
+	uint8_t __pad0x18[0x2];		//0x18
+	uint8_t flags;				//0x1A
+	uint8_t __pad0x1B[0x9];		//0x1B
+}; //0x24
