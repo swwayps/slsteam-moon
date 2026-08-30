DeveloperMikey/SLSsteam

fix(fakeAppIds): Fix some callbacks not reaching fakeAppId enabled games

src/hooks.cpp
@@ -537,6 +537,33 @@ static uint32_t hkUser_GetSubscribedApps(void* pClientUser, AppId_t* pAppList, u
 	return count;
 }
 
+static uint32_t hkUser_PostCallbackToAppId(void* pUser, AppId_t appId, uint32_t type, void* pCallback, uint32_t callbackSize)
+{
+	const AppId_t fakeAppId = FakeAppIds::getFakeAppId(appId);
+	if (fakeAppId)
+	{
+		g_pLog->debug("Rerouting callback from %u to %u\n", appId, fakeAppId);
+		appId = fakeAppId;
+	}
+
+	const uint32_t ret = Hooks::CUser_PostCallbackToAppId.tramp.fn(pUser, appId, type, pCallback, callbackSize);
+
+	g_pLog->debug
+	(
+		"%s(%p, %u, %u, %p, %u) -> %u\n",
+
+		Hooks::CUser_PostCallbackToAppId.name.c_str(),
+		pUser,
+		appId,
+		type,
+		pCallback,
+		callbackSize,
+		ret
+	);
+
+	return ret;
+}
+
 static bool hkUserAppManager_BuildDepotDependency
 (
 	void* pClientAppManager,
@@ -1028,6 +1055,7 @@ namespace Hooks
 
 	DetourHook<CUser_CheckAppOwnership_t> CUser_CheckAppOwnership;
 	DetourHook<CUser_GetSubscribedApps_t> CUser_GetSubscribedApps;
+	DetourHook<CUser_PostCallbackToAppId_t> CUser_PostCallbackToAppId;
 
 	DetourHook<CUserAppManager_BuildDepotDependency_t> CUserAppManager_BuildDepotDependency;
 
@@ -1123,6 +1151,7 @@ bool Hooks::setup()
 
 		&& CUser_CheckAppOwnership.setup(Patterns::CUser::CheckAppOwnership, hkUser_CheckAppOwnership)
 		&& CUser_GetSubscribedApps.setup(Patterns::CUser::GetSubscribedApps, hkUser_GetSubscribedApps)
+		&& CUser_PostCallbackToAppId.setup(Patterns::CUser::PostCallbackToAppId, hkUser_PostCallbackToAppId)
 
 		&& CUserAppManager_BuildDepotDependency.setup(Patterns::CUserAppManager::BuildDepotDependency, hkUserAppManager_BuildDepotDependency)
 
@@ -1160,6 +1189,7 @@ void Hooks::place()
 
 	CUser_CheckAppOwnership.place();
 	CUser_GetSubscribedApps.place();
+	CUser_PostCallbackToAppId.place();
 
 	CUserAppManager_BuildDepotDependency.place();
 
@@ -1296,6 +1326,7 @@ void Hooks::remove()
 
 	CUser_CheckAppOwnership.remove();
 	CUser_GetSubscribedApps.remove();
+	CUser_PostCallbackToAppId.remove();
 
 	CUserAppManager_BuildDepotDependency.remove();
 

src/hooks.hpp
@@ -105,6 +105,7 @@ namespace Hooks
 
 	typedef uint32_t(*CUser_CheckAppOwnership_t)(void*, AppId_t, AppOwnershipInfo_t*);
 	typedef uint32_t(*CUser_GetSubscribedApps_t)(void*, AppId_t*, uint32_t, uint8_t);
+	typedef uint32_t(*CUser_PostCallbackToAppId_t)(void*, AppId_t, uint32_t, void*, uint32_t);
 
 	typedef bool(*CUserAppManager_BuildDepotDependency_t)(void*, AppId_t, void*, CUtlVector<DepotInfo_t>*, CUtlVector<DepotInfo_t>*, void*, uint32_t*, bool*);
 
@@ -132,6 +133,7 @@ namespace Hooks
 
 	extern DetourHook<CUser_CheckAppOwnership_t> CUser_CheckAppOwnership;
 	extern DetourHook<CUser_GetSubscribedApps_t> CUser_GetSubscribedApps;
+	extern DetourHook<CUser_PostCallbackToAppId_t> CUser_PostCallbackToAppId;
 
 	extern DetourHook<CUserAppManager_BuildDepotDependency_t> CUserAppManager_BuildDepotDependency;
 

swwayps/slsteam-moon

(cherry picked from commit 50c439a3ac65ba18069eb7e433b73b4d863a2ae9)

src/hooks.cpp
@@ -447,6 +447,33 @@ static uint32_t hkUser_GetSubscribedApps(void* pClientUser, uint32_t* pAppList, 
 	return count;
 }
 
+static uint32_t hkUser_PostCallbackToAppId(void* pUser, uint32_t appId, uint32_t type, void* pCallback, uint32_t callbackSize)
+{
+	const uint32_t fakeAppId = FakeAppIds::getFakeAppId(appId);
+	if (fakeAppId)
+	{
+		g_pLog->debug("Rerouting callback from %u to %u\n", appId, fakeAppId);
+		appId = fakeAppId;
+	}
+
+	const uint32_t ret = Hooks::CUser_PostCallbackToAppId.tramp.fn(pUser, appId, type, pCallback, callbackSize);
+
+	g_pLog->debug
+	(
+		"%s(%p, %u, %u, %p, %u) -> %u\n",
+
+		Hooks::CUser_PostCallbackToAppId.name.c_str(),
+		pUser,
+		appId,
+		type,
+		pCallback,
+		callbackSize,
+		ret
+	);
+
+	return ret;
+}
+
 static bool hkClientAppManager_BCanRemotePlayTogether(void* pClientAppManager, uint32_t appId)
 {
 	const bool ret = Hooks::IClientAppManager_BCanRemotePlayTogether.tramp.fn(pClientAppManager, appId);
@@ -1220,6 +1247,7 @@ namespace Hooks
 
 	DetourHook<CUser_CheckAppOwnership_t> CUser_CheckAppOwnership;
 	DetourHook<CUser_GetSubscribedApps_t> CUser_GetSubscribedApps;
+	DetourHook<CUser_PostCallbackToAppId_t> CUser_PostCallbackToAppId;
 
 	DetourHook<IClientAppManager_BCanRemotePlayTogether_t> IClientAppManager_BCanRemotePlayTogether;
 
@@ -1274,6 +1302,7 @@ bool Hooks::setup()
 
 		&& CUser_CheckAppOwnership.setup(Patterns::CUser::CheckAppOwnership, &hkUser_CheckAppOwnership)
 		&& CUser_GetSubscribedApps.setup(Patterns::CUser::GetSubscribedApps, &hkUser_GetSubscribedApps)
+		&& CUser_PostCallbackToAppId.setup(Patterns::CUser::PostCallbackToAppId, &hkUser_PostCallbackToAppId)
 
 		&& CSteamEngine_Init.setup(Patterns::CSteamEngine::Init, &hkSteamEngine_Init)
 		&& CSteamEngine_SetAppIdForCurrentPipe.setup(Patterns::CSteamEngine::SetAppIdForCurrentPipe, &hkSteamEngine_SetAppIdForCurrentPipe)
@@ -1361,6 +1390,7 @@ void Hooks::place()
 
 	CUser_CheckAppOwnership.place();
 	CUser_GetSubscribedApps.place();
+	CUser_PostCallbackToAppId.place();
 
 	IClientAppManager_BCanRemotePlayTogether.place();
 
@@ -1430,6 +1460,7 @@ void Hooks::remove()
 
 	CUser_CheckAppOwnership.remove();
 	CUser_GetSubscribedApps.remove();
+	CUser_PostCallbackToAppId.remove();
 
 	IClientAppManager_BCanRemotePlayTogether.remove();
 

src/hooks.hpp
@@ -94,6 +94,7 @@ namespace Hooks
 
 	typedef uint32_t(*CUser_CheckAppOwnership_t)(void*, uint32_t, CAppOwnershipInfo*);
 	typedef uint32_t(*CUser_GetSubscribedApps_t)(void*, uint32_t*, uint32_t, uint8_t);
+	typedef uint32_t(*CUser_PostCallbackToAppId_t)(void*, uint32_t, uint32_t, void*, uint32_t);
 	typedef bool(*IClientAppManager_BCanRemotePlayTogether_t)(void*, uint32_t);
 
 	typedef bool(*IClientUser_BLoggedOn_t)(void*);
@@ -139,6 +140,7 @@ namespace Hooks
 
 	extern DetourHook<CUser_CheckAppOwnership_t> CUser_CheckAppOwnership;
 	extern DetourHook<CUser_GetSubscribedApps_t> CUser_GetSubscribedApps;
+	extern DetourHook<CUser_PostCallbackToAppId_t> CUser_PostCallbackToAppId;
 
 	extern DetourHook<IClientAppManager_BCanRemotePlayTogether_t> IClientAppManager_BCanRemotePlayTogether;
 