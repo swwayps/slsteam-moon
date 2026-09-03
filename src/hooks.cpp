DeveloperMikey/SLSsteam

refactor(hooks): Check for has_target_job_name instead of just using it

src/hooks.cpp
@@ -298,7 +298,13 @@ static void hkCMInterface_RecvPkt(void* pCMInterface, CNetPacket* pNetPacket)
 			return;
 		}
 
-		if (disableFamilyShareLock && type == k_EMsgServiceMethod && header.target_job_name() == "FamilyGroupsClient.NotifyRunningApps#1")
+		if
+		(
+			disableFamilyShareLock
+			&& type == k_EMsgServiceMethod
+			&& header.has_target_job_name() //Do not modify header by blindly requesting the target_job_name
+			&& header.target_job_name() == "FamilyGroupsClient.NotifyRunningApps#1"
+		)
 		{
 			g_pLog->debug("Choking %s\n", header.target_job_name().c_str());
 			pNetPacket->free();

swwayps/slsteam-moon

(cherry picked from commit 3d7d3c6634af09970bbad7e185c917cb56c1b7d2)

src/hooks.cpp
@@ -315,7 +315,13 @@ static void hkCMInterface_RecvPkt(void* pCMInterface, CNetPacket* pNetPacket)
 			return;
 		}
 
-		if (disableFamilyShareLock && type == EMSG_SERVICE_METHOD && header.target_job_name() == "FamilyGroupsClient.NotifyRunningApps#1")
+		if
+		(
+			disableFamilyShareLock
+			&& type == EMSG_SERVICE_METHOD
+			&& header.has_target_job_name() //Do not modify header by blindly requesting the target_job_name
+			&& header.target_job_name() == "FamilyGroupsClient.NotifyRunningApps#1"
+		)
 		{
 			g_pLog->debug("Choking FamilyGroupsClient.NotifyRunningApps#1\n");
 			pNetPacket->free();