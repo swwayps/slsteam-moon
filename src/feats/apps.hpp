DeveloperMikey/SLSsteam

refactor(hooks, apps & fakeAppIds): Replace CProtoBufMsgBase::Send with CWebSocketConnection::BBuildAndAsyncSendFrame

src/feats/apps.hpp
@@ -3,7 +3,7 @@
 #include <cstdint>
 #include <unordered_set>
 
-class CProtoBufMsgBase;
+class CNetPacket;
 class CMsgClientGamesPlayed;
 class CMsgClientPICSProductInfoRequest;
 class CMsgClientPICSProductInfoResponse;
@@ -34,7 +34,7 @@ namespace Apps
 	bool shouldDisableUpdates(uint32_t appId);
 
 	void sendAndRecvLastPlayedTimes(const char* name, CPlayer_GetLastPlayedTimes_Response* recv);
-	void sendGamesPlayed(CMsgClientGamesPlayed* msg);
-	void sendPICSInfoRequest(CMsgClientPICSProductInfoRequest* msg);
-	void sendMsg(CProtoBufMsgBase* msg);
+	void sendGamesPlayed(CNetPacket* pkg);
+	void sendPICSInfoRequest(CNetPacket* pkg);
+	void sendMsg(CNetPacket* packet);
 };

src/feats/fakeappid.hpp
@@ -3,7 +3,7 @@
 #include <cstdint>
 #include <unordered_map>
 
-class CProtoBufMsgBase;
+class CNetPacket;
 
 struct gameserverdetails_t;
 struct servernetadr_t;
@@ -29,5 +29,5 @@ namespace FakeAppIds
 	uint32_t requestInternetServerList(uint32_t appId);
 	void pingResponse(gameserverdetails_t* details);
 
-	void sendMsg(CProtoBufMsgBase* msg);
+	void sendMsg(CNetPacket* pkt);
 }

swwayps/slsteam-moon

(cherry picked from commit 9cc02d3d0de0fd6ac01e486ba65c2c7eadb47962)

src/feats/apps.hpp
@@ -6,9 +6,7 @@
 #include <vector>
 
 class CAppOwnershipInfo;
-class CProtoBufMsgBase;
-class CMsgClientGamesPlayed;
-class CMsgClientPICSProductInfoRequest;
+class CNetPacket;
 
 namespace Apps
 {
@@ -56,7 +54,7 @@ namespace Apps
 	bool shouldDisableCDKey(uint32_t appId);
 	bool shouldDisableUpdates(uint32_t appId);
 
-	void sendGamesPlayed(CMsgClientGamesPlayed* msg);
-	void sendPICSInfoRequest(CMsgClientPICSProductInfoRequest* msg);
-	void sendMsg(CProtoBufMsgBase* msg);
+	void sendGamesPlayed(CNetPacket* packet);
+	void sendPICSInfoRequest(CNetPacket* packet);
+	void sendMsg(CNetPacket* packet);
 };

src/feats/fakeappid.hpp
@@ -3,7 +3,7 @@
 #include <cstdint>
 #include <unordered_map>
 
-class CProtoBufMsgBase;
+class CNetPacket;
 
 struct gameserverdetails_t;
 struct servernetadr_t;
@@ -29,7 +29,7 @@ namespace FakeAppIds
 	uint32_t requestInternetServerList(uint32_t appId);
 	void pingResponse(gameserverdetails_t* details);
 
-	void sendGamesPlayed(CProtoBufMsgBase* msg);
-	void sendRichPresenceUpload(CProtoBufMsgBase* msg);
-	void sendMsg(CProtoBufMsgBase* msg);
+	void sendGamesPlayed(CNetPacket* packet);
+	void sendRichPresenceUpload(CNetPacket* packet);
+	void sendMsg(CNetPacket* packet);
 }