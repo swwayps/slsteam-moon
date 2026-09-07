DeveloperMikey/SLSsteam

refactor(hooks): Replace naked IClientUser::GetSteamID hook with CSteamEngine::RunInterface

src/feats/apps.cpp
@@ -29,7 +29,7 @@ bool Apps::unlockApp(const AppId_t appId, AppOwnershipInfo_t* info, const uint32
 	//Changing the purchased field is enough, but just for nicety in the Steamclient UI we change the owner too
 	info->owner = ownerId;
 	info->realOwner = 0;
-	info->familyShared = ownerId != g_currentSteamId;
+	info->familyShared = ownerId != g_currentSteamId.accountId;
 
 	info->licensePermanent = !info->familyShared;
 	info->retailLicense = false;
@@ -55,7 +55,7 @@ bool Apps::unlockApp(const AppId_t appId, AppOwnershipInfo_t* info, const uint32
 
 bool Apps::unlockApp(const AppId_t appId, AppOwnershipInfo_t* info)
 {
-	return unlockApp(appId, info, g_currentSteamId);
+	return unlockApp(appId, info, g_currentSteamId.accountId);
 }
 
 void Apps::buildDepotDependency(const AppId_t appId, CUtlVector<DepotInfo_t>* depots, CUtlVector<DepotInfo_t>* sharedDepots)
@@ -97,7 +97,7 @@ bool Apps::checkAppOwnership(AppId_t appId, AppOwnershipInfo_t* pInfo)
 {
 	//Wait Until GetSubscribedApps gets called once to let Steam request and populate legit data first.
 	//Afterwards modifying should hopefully not affect false positives anymore
-	if (!applistRequested || !pInfo || !g_currentSteamId)
+	if (!applistRequested || !pInfo || !g_currentSteamId.accountId)
 	{
 		return false;
 	}
@@ -105,7 +105,7 @@ bool Apps::checkAppOwnership(AppId_t appId, AppOwnershipInfo_t* pInfo)
 	const uint32_t denuvoOwner = g_config.getDenuvoGameOwner(appId);
 
 	//Do not modify Denuvo enabled Games
-	if (denuvoOwner && denuvoOwner != g_currentSteamId)
+	if (denuvoOwner && denuvoOwner != g_currentSteamId.accountId)
 	{
 		//Would love to log the SteamId, but for users anonymity I won't
 		g_pLog->once("Skipping %u because it's a Denuvo game from someone else\n", appId);

src/feats/ticket.cpp
@@ -89,7 +89,7 @@ bool Ticket::saveTicketToCache(const CMsgClientGetAppOwnershipTicketResponse& re
 	YAML::Emitter node;
 	node << YAML::BeginMap;
 	node << YAML::Key << "steamId";
-	node << YAML::Value << g_currentSteamId;
+	node << YAML::Value << g_currentSteamId.accountId;
 	node << YAML::Key << "ticket";
 	node << YAML::Value << base64::to_base64(bytes);
 	node << YAML::EndMap;
@@ -199,7 +199,7 @@ bool Ticket::saveEncryptedTicketToCache(const CMsgClientRequestEncryptedAppTicke
 	YAML::Emitter node;
 	node << YAML::BeginMap;
 	node << YAML::Key << "steamId";
-	node << YAML::Value << g_currentSteamId;
+	node << YAML::Value << g_currentSteamId.accountId;
 	node << YAML::Key << "encryptedTicket";
 	//node << YAML::Value << YAML::EncodeBase64(reinterpret_cast<const unsigned char*>(bytes.c_str()), bytes.size());
 	node << YAML::Value << base64::to_base64(bytes);
@@ -214,7 +214,7 @@ bool Ticket::saveEncryptedTicketToCache(const CMsgClientRequestEncryptedAppTicke
 
 	//TODO: Skip copy
 	SavedTicket ticket {};
-	ticket.steamId = g_currentSteamId;
+	ticket.steamId = g_currentSteamId.accountId;
 	ticket.ticket = bytes;
 	encryptedTicketMap[appId] = ticket;
 	

src/globals.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "sdk/steam.hpp"
+
 #include "libmem/libmem.h"
 
 #include <cstdint>
@@ -8,6 +10,4 @@
 extern lm_module_t g_modSteamClient;
 extern lm_module_t g_modSteamUI;
 
-//Don't assign a pointer to IClientUser::GetSteamID! It's returned pointer
-//always points to the same address, but it's lifetime is very short
-extern uint32_t g_currentSteamId;
+extern CSteamId g_currentSteamId;

swwayps/slsteam-moon

(cherry picked from commit c4152f51c2aff4af8e0ae37d5c96ba9c297048da)

src/feats/apps.cpp
@@ -211,7 +211,7 @@ bool Apps::unlockApp(uint32_t appId, CAppOwnershipInfo* info, uint32_t ownerId)
 {
 	info->owner = ownerId;
 	info->realOwner = 0;
-	info->familyShared = ownerId != g_currentSteamId;
+	info->familyShared = ownerId != g_currentSteamId.steamId;
 
 	info->licensePermanent = !info->familyShared;
 	info->retailLicense = false;
@@ -237,19 +237,19 @@ bool Apps::unlockApp(uint32_t appId, CAppOwnershipInfo* info, uint32_t ownerId)
 
 bool Apps::unlockApp(uint32_t appId, CAppOwnershipInfo* info)
 {
-	return unlockApp(appId, info, g_currentSteamId);
+	return unlockApp(appId, info, g_currentSteamId.steamId);
 }
 
 bool Apps::checkAppOwnership(uint32_t appId, CAppOwnershipInfo* pInfo)
 {
-	if (!applistRequested || !pInfo || !g_currentSteamId)
+	if (!applistRequested || !pInfo || !g_currentSteamId.steamId)
 	{
 		return false;
 	}
 
 	const uint32_t denuvoOwner = g_config.getDenuvoGameOwner(appId);
 
-	if (denuvoOwner && denuvoOwner != g_currentSteamId)
+	if (denuvoOwner && denuvoOwner != g_currentSteamId.steamId)
 	{
 		g_pLog->infoOnce("Skipping %u because it's a Denuvo game from someone else\n", appId);
 		return false;

src/feats/ticket.cpp
@@ -94,7 +94,7 @@ bool Ticket::saveTicketToCache(CMsgClientGetAppOwnershipTicketResponse* resp)
 	YAML::Emitter node;
 	node << YAML::BeginMap;
 	node << YAML::Key << "steamId";
-	node << YAML::Value << g_currentSteamId;
+	node << YAML::Value << g_currentSteamId.steamId;
 	node << YAML::Key << "ticket";
 	node << YAML::Value << base64::to_base64(bytes);
 	node << YAML::EndMap;
@@ -213,7 +213,7 @@ bool Ticket::saveEncryptedTicketToCache(CMsgClientRequestEncryptedAppTicketRespo
 	YAML::Emitter node;
 	node << YAML::BeginMap;
 	node << YAML::Key << "steamId";
-	node << YAML::Value << g_currentSteamId;
+	node << YAML::Value << g_currentSteamId.steamId;
 	node << YAML::Key << "encryptedTicket";
 	//node << YAML::Value << YAML::EncodeBase64(reinterpret_cast<const unsigned char*>(bytes.c_str()), bytes.size());
 	node << YAML::Value << base64::to_base64(bytes);
@@ -229,7 +229,7 @@ bool Ticket::saveEncryptedTicketToCache(CMsgClientRequestEncryptedAppTicketRespo
 
 	//TODO: Skip copy
 	SavedTicket ticket {};
-	ticket.steamId = g_currentSteamId;
+	ticket.steamId = g_currentSteamId.steamId;
 	ticket.ticket = bytes;
 	encryptedTicketMap[appId] = ticket;
 	return true;

src/globals.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "sdk/CSteamID.hpp"
+
 #include "libmem/libmem.h"
 
 #include <cstdint>
@@ -9,6 +11,4 @@
 extern lm_module_t g_modSteamClient;
 extern lm_module_t g_modSteamUI;
 
-//Don't assign a pointer to IClientUser::GetSteamID! It's returned pointer
-//always points to the same address, but it's lifetime is very short
-extern uint32_t g_currentSteamId;
+extern CSteamId g_currentSteamId;