DeveloperMikey/SLSsteam

feat(ticket): Only spoof steamId when necessary

src/hooks.cpp
@@ -801,6 +801,30 @@ static uint32_t hkClientUser_GetAppOwnershipTicketExtendedData
 	return ret;
 }
 
+static bool hkClientUser_GetEncryptedAppTicket(void* pClientUser, void* pTicket, uint32_t ticketSize, uint32_t* pTicketSize)
+{
+	const bool success = Hooks::IClientUser_GetEncryptedAppTicket.originalFn.fn(pClientUser, pTicket, ticketSize, pTicketSize);
+
+	g_pLog->debug
+	(
+		"%s(%p, %p, %u, %p) -> %u\n",
+
+		Hooks::IClientUser_GetEncryptedAppTicket.name.c_str(),
+		pClientUser,
+		pTicket,
+		ticketSize,
+		pTicketSize,
+		success
+	);
+
+	if (success)
+	{
+		Ticket::getEncryptedAppTicket(FakeAppIds::getRealAppIdForCurrentPipe());
+	}
+
+	return success;
+}
+
 static uint8_t hkClientUser_IsUserSubscribedAppInTicket(void* pClientUser, uint32_t steamId, uint32_t a2, uint32_t a3, AppId_t appId)
 {
 	const uint8_t ticketState = Hooks::IClientUser_IsUserSubscribedAppInTicket.originalFn.fn(pClientUser, steamId, a2, a3, appId);
@@ -842,25 +866,15 @@ static CSteamId hkClientUser_GetSteamId(const CSteamId& steamId)
 		return steamId;
 	}
 
-	CSteamId newId = steamId;
-
-	//Use Pipe AppId since getCachedEncryptedTicket handles logic for FakeAppIds itself
-	Ticket::SavedTicket ticket = Ticket::getCachedEncryptedTicket(utils->getAppId());
-
-	//One time spoof should take presedence, otherwise SteamStub will fail
-	//for games that use encrypted tickets for online auth when you play on multiple accounts
 	if (Ticket::oneTimeSteamIdSpoof.isSet())
 	{
-		//One time spoof should be enough for this type
-		newId = Ticket::oneTimeSteamIdSpoof;
+		const CSteamId newId = Ticket::oneTimeSteamIdSpoof;
 		Ticket::oneTimeSteamIdSpoof.steamId64 = 0;
-	}
-	else if (ticket.isValid())
-	{
-		newId = ticket.steamId;
+
+		return newId;
 	}
 
-	return newId;
+	return steamId;
 }
 
 static bool hkClientUser_RequiresLegacyCDKey(void* pClientUser, AppId_t appId, uint32_t* a2)
@@ -991,6 +1005,7 @@ namespace Hooks
 	VFTHook<IClientUser_BLoggedOn_t> IClientUser_BLoggedOn;
 	VFTHook<IClientUser_BUpdateAppOwnershipTicket_t> IClientUser_BUpdateAppOwnershipTicket;
 	VFTHook<IClientUser_GetAppOwnershipTicketExtendedData_t> IClientUser_GetAppOwnershipTicketExtendedData;
+	VFTHook<IClientUser_GetEncryptedAppTicket_t> IClientUser_GetEncryptedAppTicket;
 	VFTHook<IClientUser_IsUserSubscribedAppInTicket_t> IClientUser_IsUserSubscribedAppInTicket;
 	VFTHook<IClientUser_RequiresLegacyCDKey_t> IClientUser_RequiresLegacyCDKey;
 
@@ -1167,12 +1182,16 @@ void Hooks::placeVFTHooks()
 		Hooks::IClientUser_BLoggedOn.setup(vft, VFTIndexes::IClientUser::BLoggedOn, &hkClientUser_BLoggedOn);
 		Hooks::IClientUser_BUpdateAppOwnershipTicket.setup(vft, VFTIndexes::IClientUser::BUpdateAppOwnershipTicket, hkClientUser_BUpdateOwnershipTicket);
 		Hooks::IClientUser_GetAppOwnershipTicketExtendedData.setup(vft, VFTIndexes::IClientUser::GetAppOwnershipTicketExtendedData, hkClientUser_GetAppOwnershipTicketExtendedData);
-		Hooks::IClientUser_IsUserSubscribedAppInTicket.setup(vft, VFTIndexes::IClientUser::IsUserSubscribedAppInTicket, &hkClientUser_IsUserSubscribedAppInTicket);
+		//GetEncryptedAppTicket is just a wrapper for CUser::GetEncryptedAppTicket. But there is no need to go deeper
+		//since we load the encrypted ticket in the Networking layer. We just need this function to spoof our steamId once
+		Hooks::IClientUser_GetEncryptedAppTicket.setup(vft, VFTIndexes::IClientUser::GetEncryptedAppTicket, hkClientUser_GetEncryptedAppTicket);
+		Hooks::IClientUser_IsUserSubscribedAppInTicket.setup(vft, VFTIndexes::IClientUser::IsUserSubscribedAppInTicket, hkClientUser_IsUserSubscribedAppInTicket);
 		Hooks::IClientUser_RequiresLegacyCDKey.setup(vft, VFTIndexes::IClientUser::RequiresLegacyCDKey, hkClientUser_RequiresLegacyCDKey);
 
 		Hooks::IClientUser_BLoggedOn.place();
 		Hooks::IClientUser_BUpdateAppOwnershipTicket.place();
 		Hooks::IClientUser_GetAppOwnershipTicketExtendedData.place();
+		Hooks::IClientUser_GetEncryptedAppTicket.place();
 		Hooks::IClientUser_IsUserSubscribedAppInTicket.place();
 		Hooks::IClientUser_RequiresLegacyCDKey.place();
 
@@ -1243,6 +1262,7 @@ void Hooks::remove()
 	IClientUser_BLoggedOn.remove();
 	IClientUser_BUpdateAppOwnershipTicket.remove();
 	IClientUser_GetAppOwnershipTicketExtendedData.remove();
+	IClientUser_GetEncryptedAppTicket.remove();
 	IClientUser_IsUserSubscribedAppInTicket.remove();
 	IClientUser_RequiresLegacyCDKey.remove();
 

src/hooks.hpp
@@ -153,6 +153,7 @@ namespace Hooks
 	typedef bool(*IClientUser_BLoggedOn_t)(void*);
 	typedef uint32_t(*IClientUser_BUpdateAppOwnershipTicket_t)(void*, AppId_t, bool);
 	typedef uint32_t(*IClientUser_GetAppOwnershipTicketExtendedData_t)(void*, uint32_t, void*, uint32_t, uint32_t*, uint32_t*, uint32_t*, uint32_t*);
+	typedef bool(*IClientUser_GetEncryptedAppTicket_t)(void*, void*, uint32_t, uint32_t*);
 	typedef uint8_t(*IClientUser_IsUserSubscribedAppInTicket_t)(void*, uint32_t, uint32_t, uint32_t, AppId_t);
 	typedef bool(*IClientUser_RequiresLegacyCDKey_t)(void*, AppId_t, uint32_t*);
 
@@ -171,6 +172,7 @@ namespace Hooks
 	extern VFTHook<IClientUser_BLoggedOn_t> IClientUser_BLoggedOn;
 	extern VFTHook<IClientUser_BUpdateAppOwnershipTicket_t> IClientUser_BUpdateAppOwnershipTicket;
 	extern VFTHook<IClientUser_GetAppOwnershipTicketExtendedData_t> IClientUser_GetAppOwnershipTicketExtendedData;
+	extern VFTHook<IClientUser_GetEncryptedAppTicket_t> IClientUser_GetEncryptedAppTicket;
 	extern VFTHook<IClientUser_IsUserSubscribedAppInTicket_t> IClientUser_IsUserSubscribedAppInTicket;
 	extern VFTHook<IClientUser_RequiresLegacyCDKey_t> IClientUser_RequiresLegacyCDKey;
 

swwayps/slsteam-moon

(cherry picked from commit 3c520bd95f03b19e1617f95637107650bd713545)

src/hooks.cpp
@@ -1164,7 +1164,31 @@ static uint32_t hkClientUser_GetAppOwnershipTicketExtendedData(
 	return ret;
 }
 
-static uint8_t hkClientUser_IsUserSubscribedAppInTicket(void* pClientUser, uint32_t steamId, uint32_t a2, uint32_t a3, uint32_t appId)
+static bool hkClientUser_GetEncryptedAppTicket(void* pClientUser, void* pTicket, uint32_t ticketSize, uint32_t* pTicketSize)
+{
+	const bool success = Hooks::IClientUser_GetEncryptedAppTicket.originalFn.fn(pClientUser, pTicket, ticketSize, pTicketSize);
+
+	g_pLog->debug
+	(
+		"%s(%p, %p, %u, %p) -> %u\n",
+
+		Hooks::IClientUser_GetEncryptedAppTicket.name.c_str(),
+		pClientUser,
+		pTicket,
+		ticketSize,
+		pTicketSize,
+		success
+	);
+
+	if (success)
+	{
+		Ticket::getEncryptedAppTicket(FakeAppIds::getRealAppIdForCurrentPipe());
+	}
+
+	return success;
+}
+
+static uint8_t hkClientUser_IsUserSubscribedAppInTicket(void* pClientUser, uint32_t steamId, uint32_t a2, uint32_t a3, AppId_t appId)
 {
 	const uint8_t ticketState = Hooks::IClientUser_IsUserSubscribedAppInTicket.tramp.fn(pClientUser, steamId, a2, a3, appId);
 	g_pLog->debug
@@ -1202,25 +1226,15 @@ static CSteamId hkClientUser_GetSteamId(const CSteamId& steamId)
 		return steamId;
 	}
 
-	CSteamId newId = steamId;
-
-	//Use Pipe AppId since getCachedEncryptedTicket handles logic for FakeAppIds itself
-	Ticket::SavedTicket ticket = Ticket::getCachedEncryptedTicket(utils->getAppId());
-
-	//One time spoof should take presedence, otherwise SteamStub will fail
-	//for games that use encrypted tickets for online auth when you play on multiple accounts
 	if (Ticket::oneTimeSteamIdSpoof.isSet())
 	{
-		//One time spoof should be enough for this type
-		newId = Ticket::oneTimeSteamIdSpoof;
+		const CSteamId newId = Ticket::oneTimeSteamIdSpoof;
 		Ticket::oneTimeSteamIdSpoof.steamId64 = 0;
-	}
-	else if (ticket.isValid())
-	{
-		newId = ticket.steamId;
+
+		return newId;
 	}
 
-	return newId;
+	return steamId;
 }
 
 static bool hkClientUser_RequiresLegacyCDKey(void* pClientUser, uint32_t appId, uint32_t* a2)
@@ -1344,6 +1358,7 @@ namespace Hooks
 	DetourHook<IClientUser_BLoggedOn_t> IClientUser_BLoggedOn;
 	DetourHook<IClientUser_BUpdateAppOwnershipTicket_t> IClientUser_BUpdateAppOwnershipTicket;
 	DetourHook<IClientUser_GetAppOwnershipTicketExtendedData_t> IClientUser_GetAppOwnershipTicketExtendedData;
+	DetourHook<IClientUser_GetEncryptedAppTicket_t> IClientUser_GetEncryptedAppTicket;
 	DetourHook<IClientUser_IsUserSubscribedAppInTicket_t> IClientUser_IsUserSubscribedAppInTicket;
 	DetourHook<IClientUser_RequiresLegacyCDKey_t> IClientUser_RequiresLegacyCDKey;
 

src/hooks.hpp
@@ -108,6 +108,7 @@ namespace Hooks
 	typedef bool(*IClientUser_BLoggedOn_t)(void*);
 	typedef uint32_t(*IClientUser_BUpdateAppOwnershipTicket_t)(void*, uint32_t, bool);
 	typedef uint32_t(*IClientUser_GetAppOwnershipTicketExtendedData_t)(void*, uint32_t, void*, uint32_t, uint32_t*, uint32_t*, uint32_t*, uint32_t*);
+	typedef bool(*IClientUser_GetEncryptedAppTicket_t)(void*, void*, uint32_t, uint32_t*);
 	typedef uint8_t(*IClientUser_IsUserSubscribedAppInTicket_t)(void*, uint32_t, uint32_t, uint32_t, uint32_t);
 	typedef bool(*IClientUser_RequiresLegacyCDKey_t)(void*, uint32_t, uint32_t*);
 
@@ -159,6 +160,7 @@ namespace Hooks
 	extern DetourHook<IClientUser_BLoggedOn_t> IClientUser_BLoggedOn;
 	extern DetourHook<IClientUser_BUpdateAppOwnershipTicket_t> IClientUser_BUpdateAppOwnershipTicket;
 	extern DetourHook<IClientUser_GetAppOwnershipTicketExtendedData_t> IClientUser_GetAppOwnershipTicketExtendedData;
+	extern DetourHook<IClientUser_GetEncryptedAppTicket_t> IClientUser_GetEncryptedAppTicket;
 	extern DetourHook<IClientUser_IsUserSubscribedAppInTicket_t> IClientUser_IsUserSubscribedAppInTicket;
 	extern DetourHook<IClientUser_RequiresLegacyCDKey_t> IClientUser_RequiresLegacyCDKey;
 