DeveloperMikey/SLSsteam

feat(config): Add SteamIdOverride option

src/hooks.cpp
@@ -869,6 +869,28 @@ static CSteamId hkClientUser_GetSteamId(const CSteamId& steamId)
 		return steamId;
 	}
 
+	const auto overrides = g_config.steamIdOverride.get();
+	if (overrides.contains(realAppId))
+	{
+		const uint64_t& id64 = overrides.at(realAppId);
+		if (id64)
+		{
+			return CSteamId(id64);
+		}
+
+		const auto cached = Ticket::getCachedTicket(realAppId);
+		if (cached.isValid())
+		{
+			return cached.steamId;
+		}
+
+		g_pLog->once
+		(
+			"SteamIdOverride for %u is set with automatic mode, but no AppOwnershipTicket exists in cache! Falling through to normal operation\n",
+			realAppId
+		);
+	}
+
 	if (Ticket::oneTimeSteamIdSpoof.contains(realAppId))
 	{
 		const CSteamId newId = Ticket::oneTimeSteamIdSpoof.at(realAppId);

swwayps/slsteam-moon

(cherry picked from commit f7926b5727c2e2f5dd1f4be9085c7de9e4f76fe9)

src/hooks.cpp
@@ -1229,6 +1229,28 @@ static CSteamId hkClientUser_GetSteamId(const CSteamId& steamId)
 		return steamId;
 	}
 
+	const auto overrides = g_config.steamIdOverride.get();
+	if (overrides.contains(realAppId))
+	{
+		const uint64_t& id64 = overrides.at(realAppId);
+		if (id64)
+		{
+			return CSteamId(id64);
+		}
+
+		const auto cached = Ticket::getCachedTicket(realAppId);
+		if (cached.isValid())
+		{
+			return cached.steamId;
+		}
+
+		g_pLog->infoOnce
+		(
+			"SteamIdOverride for %u is set with automatic mode, but no AppOwnershipTicket exists in cache! Falling through to normal operation\n",
+			realAppId
+		);
+	}
+
 	if (Ticket::oneTimeSteamIdSpoof.contains(realAppId))
 	{
 		const CSteamId newId = Ticket::oneTimeSteamIdSpoof.at(realAppId);